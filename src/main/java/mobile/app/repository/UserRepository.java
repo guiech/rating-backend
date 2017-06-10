30dcb24f3722fc120eb5974d16904ff3cda942e1
guiech <gjaiquel@gmail.com>
2017-06-10 17:46:32 -0300

    crud capabilities added to user repository

--- src/main/java/mobile/app/repository/UserRepository.java
+++ src/main/java/mobile/app/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package mobile.app.repository;
 
 import mobile.app.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by guillermoaiquel on 6/10/17.
  */
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
 }
