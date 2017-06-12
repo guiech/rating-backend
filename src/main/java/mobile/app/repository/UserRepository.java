f94f39d44e165c5632c705a178f516be67f406f2
guiech <gjaiquel@gmail.com>
2017-06-12 20:23:44 -0300

    search user by email added

--- src/main/java/mobile/app/repository/UserRepository.java
+++ src/main/java/mobile/app/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package mobile.app.repository;
 
+import java.util.List;
+
 import mobile.app.model.User;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by guillermoaiquel on 6/10/17.
  */
 public interface UserRepository extends CrudRepository<User, String> {
+
+    @Query("{ email :  { $regex : ?0, $options: 'i'}}")
+    List<User> findByEmail(@Param("email") String email);
+
 }
