32c538c021c3240b9b5a868335b1d6bfd2d001a5
guiech <gjaiquel@gmail.com>
2017-08-22 21:04:24 -0300

    name and username fields added

--- src/main/java/mobile/app/model/User.java
+++ src/main/java/mobile/app/model/User.java
@@ -13,16 +13,36 @@ public class User {
     @Id
     public String id;
 
+    public String name;
+    public String username;
     public String email;
     public String password;
     private List<String> roles;
 
     public User() {}
 
-    public User(String email, String password, List<String> roles) {
+    public User(String name, String username, String email, String password, List<String> roles) {
+        this.name = name;
+        this.username = username;
         this.email = email;
         this.password = password;
         this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
